e3652860562f2e35fd73ef9acc91f68b098bbb83
tatiana_bahrova <gyberman@gmail.com>
2024-06-12 11:18:13 +0300

    fixed mistakes in task-1, task-2, task-3

--- js/task-3.js
+++ js/task-3.js
@@ -5,4 +5,6 @@ function getElementWidth(content, padding, border) {
     return totalWidth = contentWidth + paddingtWidth * 2 + bordertWidth * 2;
 }
 
-console.log(getElementWidth("100px", "0px", "0px"));
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
